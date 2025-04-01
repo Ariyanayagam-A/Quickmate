b50ea8e454a66669bf920b090ce389347ef8ca20
Arya <ariyanayagam.awscloud@gmail.com>
2025-04-01 11:08:59 +0530

    new tables

---
+++ database/migrations/2025_03_31_113727_2025_03_31_add_missing_columns_to_organizations_table.php.php
@@ -0,0 +1,19 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up() {
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->string('realm', 50)->nullable()->after('id'); 
+            $table->string('password', 255)->nullable()->after('token');
+        });
+    }
+
+    public function down() {
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->dropColumn(['realm', 'password']);
+        });
+    }
+};
