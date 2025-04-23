1a88661da8ee519661f1fbf7f64871de8fb1ad8a
Arya <ariyanayagam.awscloud@gmail.com>
2025-04-23 23:23:07 +0530

    adminpassword

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -114,7 +114,7 @@ class AuthController extends Controller
             'password' => 'required',
         ]);
     
-        if ($credentials['email'] == "quickmateadmin12@kloudstack.com" && $credentials['password'] == "quickmate@123") {
+        if ($credentials['email'] == "quickmateadmin12@kloudstack.com" && $credentials['password'] == "siva1@goku") {
             Session::put('superadmin_logged_in', true); // set session
             return redirect()->route('super.admin.dashboard');
         }
